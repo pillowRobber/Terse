8f3d0b879184561dd187f441d8225e3376b8e339
shan_cha <1206260484@qq.com>
2023-03-30 11:01:05 +0800

    build: add vitest module

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
@@ -26,5 +27,12 @@ export default defineConfig({
       formats: ["es", "umd", "iife"],
     },
     cssCodeSplit: true,
+  },
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    transformMode: {
+      web: [/.[tj]sx$/],
+    },
   },
 });
